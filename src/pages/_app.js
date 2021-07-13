81eab76be72681b093d2c7eec0dff70ec326ced1
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-13 10:52:50 +0900

    🚚 typescriptの導入により、ファイル名を識別子をtsxに変更

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import 'src/styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
+import { VFC } from 'react';
+
+import 'src/styles/globals.css';
+
+const MyApp: VFC = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Suzu&apos;s Blog</title>
+        <meta name='description' content='ブログサイトです。' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
+};
+
+export default MyApp;
