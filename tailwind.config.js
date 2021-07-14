6c74aab41af7d793022962d90b03f4b5626b2788
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-15 03:32:09 +0900

    🔧 tailwind css にカスタムカラーを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, 
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: colors.violet,
+      },
+    },
   },
   variants: {
     extend: {},
