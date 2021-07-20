df9ded6235a6542af64fa113a857f33e091b0009
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-20 17:11:14 +0900

    💄 ヘッダーとフッターのデザイン、カラーの変更

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { memo, VFC } from 'react';
 
 export const Footer: VFC= memo(() => {
   return (
-    <footer className='text-violet-700 bg-violet-200 w-full h-8 sm:h-10 flex justify-center items-center'>
+    <footer className='text-purple-700 bg-violet-100 w-full h-10 sm:h-10 flex justify-center items-center'>
       &copy;Chie Suzuhara 2021
     </footer>
   );
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,13 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { memo, VFC } from 'react';
 
 export const Header: VFC = memo(() => {
   return (
-    <header className='text-blue-700 font-bold bg-violet-200 w-full h-8 sm:h-10 flex items-center justify-between pr-4'>
-      <Link href='' passHref>
-        <div className='flex items-center cursor-pointer h-14 pl-4'>
-          <p>Suzu&apos;s diary</p>
+    <header className='text-purple-700 bg-violet-100 w-full h-10 sm:h-10 flex items-center justify-between pr-4'>
+      <Link href='/' passHref>
+        <div className='flex items-center cursor-pointer pl-3'>
+          <Image src='/suzu.png' width={30} height={35} alt='鈴のアイコン' className=''/>
         </div>
       </Link>
     </header>
