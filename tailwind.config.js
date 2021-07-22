68a3f0a63a49392ec4a98aaeb8bc83a8b670030f
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-22 17:42:11 +0900

    🔧 tailwindcss の拡張を設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,31 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
     extend: {
       colors: {
         violet: colors.violet,
       },
+      fontFamily: {
+        lobster: ['Lobster', 'cursive'],
+      },
       gridTemplateColumns: {
-        blogs: 'repeat(auto-fill, minmax(250px, 1fr))',
+        blogs: 'repeat(auto-fill, minmax(220px, 300px))',
+      },
+      minWidth: {
+        64: '16rem',
       },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      margin: ['hover', 'focus'],
+    },
   },
   plugins: [],
 };
