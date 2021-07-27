5364093af6e3d07bdb36141e09538826602953c1
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-27 21:51:51 +0900

    ✨ 動的ルーティングでブログの各ページを生成

---
+++ src/pages/post/[id]/index.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import { Header } from 'src/components/layout/Header';
+
+const PostId: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Header />
+      <div>ブログの子ページ</div>
+      <div>{router.query.id}</div>
+      <button
+        className='border border-gray-500 rounded-md px-3 py-2'
+        onClick={() => router.back()}
+      >
+        戻る
+      </button>
+    </div>
+  );
+};
+
+export default PostId;
