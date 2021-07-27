099059e66ee74f4e1a5eb06b838781a4834665d4
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-27 21:50:25 +0900

    ✨ ブログ一覧でmicroCMS のデータを埋め込んで表示

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -1,13 +1,55 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { VFC } from 'react';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
-export const BlogItem: VFC = () => {
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+type Props = {
+  blog: any;
+};
+
+export const BlogItem: VFC<Props> = (props) => {
+  const { blog } = props;
+  console.log(blog);
+
+  // 公開日を日本時間に加工
+  const publishedAt = dayjs
+    .utc(blog.publishedAt)
+    .tz('Asia/Tokyo')
+    .format('YYYY-MM-DD');
+  // 更新日を日本時間に加工
+  const revisedAt = dayjs
+    .utc(blog.revisedAt)
+    .tz('Asia/Tokyo')
+    .format('YYYY-MM-DD');
+  console.log(publishedAt);
+  console.log(revisedAt);
+
   return (
     <div className='text-center rounded-lg shadow-md hover:shadow-xl hover:opacity-90'>
-      <Image src='/portfolio-site.jpg' alt='アイキャッチ画像' width={300} height={180} className=''/>
-      <div>
-        <div className='text-gray-400'>🗓20xx.x.x  🏷ポートフォリオ</div>
-        <div className='font-bold p-2'>HTMLとCSSでポートフォリオを作ってみた！</div>
+      <Link href={`/post/${blog.id}`}>
+        <a>
+          <Image
+            src={blog.image.url}
+            alt='アイキャッチ画像'
+            width={300}
+            height={180}
+            className=''
+          />
+        </a>
+      </Link>
+      <div className='text-left'>
+        <div className='text-gray-400 pl-2'>
+          🗓 {publishedAt} <span className='ml-5'><FontAwesomeIcon icon={faSyncAlt} size='sm' /> {revisedAt}</span>
+        </div>
+        <p className='text-gray-400 pl-2'>🏷ポートフォリオ</p>
+        <div className='font-bold p-2'>{blog.title}</div>
       </div>
     </div>
   );
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,14 +1,20 @@
 import { VFC } from 'react';
 import { BlogItem } from './BlogItem';
 
-export const BlogList: VFC = () => {
+type Props = {
+  blogs: any;
+};
+
+export const BlogList: VFC<Props> = (props) => {
+  const { blogs } = props;
+  console.log(blogs);
+  
   return (
     <div className='text-center w-10/12 md:w-3/4 grid grid-cols-blogs gap-10 justify-center'>
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
+      {blogs &&
+        blogs.map((blog: any) => {
+          return <BlogItem key={blog.id} blog={blog} />;
+        })}
     </div>
   );
 };
