a448581b96c845d53554342f91748badd5e7dac0
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-29 12:28:19 +0900

    🚚 ファイル名に空文字が入っていたため修正

---
+++ src/components/Category.tsx
@@ -0,0 +1,16 @@
+import { memo, VFC } from 'react';
+import { CategoryItem } from 'src/components/CategoryItem';
+
+export const Category: VFC = memo(() => {
+  return (
+    <div className='w-10/12 border border-violet-200'>
+      <h1 className='p-2 text-lg text-center bg-violet-200 shadow-sm'>
+        カテゴリー
+      </h1>
+      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
+      <ul className='divide-y divide-violet-100 > *'>
+        <CategoryItem />
+      </ul>
+    </div>
+  );
+});
