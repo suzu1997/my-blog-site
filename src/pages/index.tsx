d424652d742853d53dacb1e7705ed57f86baccf1
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-29 12:27:22 +0900

    ✨ トップのブログ一覧とブログの子ページで、SSG を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
 // import Image from 'next/image';
-import { NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 import Image from 'next/image';
 import { BlogList } from 'src/components/BlogList';
 
 import { Footer } from 'src/components/layout/Footer';
 import { SideBar } from 'src/components/SideBar';
 import { Header } from 'src/components/layout/Header';
+import { fetcher } from 'src/libs/fetcher';
 
-const Home: NextPage = () => {
+type Props = {
+  blogs: any;
+};
+
+const Home: NextPage<Props> = ({ blogs }) => {
   return (
-    <div className='min-h-screen p-0 flex flex-col items-center font-serif'>
+    <div className='flex flex-col items-center p-0 min-h-screen font-serif'>
       <Header />
       <main className='flex-grow min-h-full'>
-        <div className='w-screen h-80 relative'>
+        <div className='relative w-screen h-80'>
           <div>
             <Image
               src='/top2.jpg'
@@ -22,18 +27,28 @@ const Home: NextPage = () => {
               className='opacity-70'
             />
           </div>
-          <h1 className='absolute top-2/3 left-5 sm:left-20 text-purple-700 text-5xl md:text-6xl lg:text-7xl font-bold font-lobster tracking-wide mix-blend-overlay block'>
+          <h1 className='block absolute top-2/3 left-5 sm:left-20 font-lobster text-5xl md:text-6xl lg:text-7xl font-bold tracking-wide text-purple-700 mix-blend-overlay'>
             Suzu&apos;s diary
           </h1>
         </div>
-        <div className='flex flex-col items-center my-12 md:flex-row md:justify-between md:items-start md:mx-16'>
-          <BlogList />
+        <div className='flex flex-col md:flex-row md:justify-between items-center md:items-start my-12 md:mx-16'>
+          <BlogList blogs={blogs} />
           <SideBar />
         </div>
       </main>
       <Footer />
     </div>
   );
+};
+//SSG
+export const getStaticProps: GetStaticProps = async () => {
+  const data: any = await fetcher('https://suzu-blog.microcms.io/api/v1/blog');
+
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  };
 };
 
 export default Home;
--- src/pages/post/[id]/index.tsx
+++ src/pages/post/[id]/index.tsx
@@ -1,15 +1,24 @@
-import { NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import { Header } from 'src/components/layout/Header';
+import { fetcher } from 'src/libs/fetcher';
 
-const PostId: NextPage = () => {
+type Props = {
+  post: any;
+};
+
+const PostId: NextPage<Props> = ({ post }) => {
   const router = useRouter();
+  console.log(post);
 
   return (
     <div>
       <Header />
       <div>ブログの子ページ</div>
       <div>{router.query.id}</div>
+      <div>{`タイトル:${post.title}`}</div>
+      <p>{post.publishedAt}</p>
+      <div dangerouslySetInnerHTML={{ __html: `${post.body}` }}></div>
       <button
         className='py-2 px-3 rounded-md border border-gray-500'
         onClick={() => router.back()}
@@ -18,6 +27,48 @@ const PostId: NextPage = () => {
       </button>
     </div>
   );
+};
+
+//idの一覧を取得するための関数
+export async function getAllpostsIds(): Promise<any> {
+  const posts = await fetcher('https://suzu-blog.microcms.io/api/v1/blog');
+
+  return posts.contents.map((post: any) => {
+    return {
+      params: {
+        id: String(post.id),
+      },
+    };
+  });
+}
+
+//特定のidを使って、データベースからビルド時にデータを取得するための関数
+export async function getPostData(id: string | string[] | undefined): Promise<{
+  res: any;
+}> {
+  const post = await fetcher(`https://suzu-blog.microcms.io/api/v1/blog/${id}`);
+
+  return post;
+}
+
+//idのとりうる値のリストを返す
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllpostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+//idに基づいて必要なデータを取得
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const post = await getPostData(params?.id);
+  return {
+    props: {
+      post,
+    },
+    revalidate: 5,
+  };
 };
 
 export default PostId;
