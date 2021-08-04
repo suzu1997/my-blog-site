3d0efce9bffbbe149dd581f808d7752afe46976d
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-04 15:45:43 +0900

    ✨ TopImageコンポーネントの作成

---
+++ src/components/TopImage.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/dist/client/router';
+import Image from 'next/image';
+import { memo, VFC } from 'react';
+
+export const TopImage: VFC = memo(() => {
+  const router = useRouter();
+  return (
+    <div className={`${router.pathname === '/' ? 'relative w-screen h-80' : 'relative w-screen h-40'}`}>
+      <div>
+        <Image
+          src='/top2.jpg'
+          alt='ルーマニアの街並み'
+          layout='fill'
+          objectFit='cover'
+          className='opacity-70'
+        />
+      </div>
+      <h1 className='block absolute bottom-8 left-5 sm:left-20 font-lobster text-5xl md:text-6xl lg:text-7xl font-bold tracking-wide text-purple-700 mix-blend-overlay'>
+        Suzu&apos;s diary
+      </h1>
+    </div>
+  );
+});
