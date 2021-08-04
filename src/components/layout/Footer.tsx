176de408f1461f2315cba3207416e8dab7df450b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-04 16:01:53 +0900

    💄 フッターのmarginをスマホ時のみに修正

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { memo, VFC } from 'react';
 
 export const Footer: VFC = memo(() => {
   return (
-    <footer className='flex justify-center items-center sm:mb-20 w-full h-10 sm:h-10 text-purple-700 bg-violet-100'>
+    <footer className='flex justify-center items-center mb-20 sm:mb-0 w-full h-10 sm:h-10 text-purple-700 bg-violet-100'>
       &copy;Chie Suzuhara 2021
     </footer>
   );
