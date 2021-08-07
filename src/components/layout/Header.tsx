91b28af0e26e88d1e88668e822223c52c4e9fe3a
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-07 16:26:00 +0900

    ✨ デスクトップサイズのヘッダーに、トップへ戻るボタンを追加

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,10 +6,16 @@ export const Header: VFC = memo(() => {
   return (
     <header className='flex justify-between items-center pr-4 w-full h-10 sm:h-10 text-purple-700 bg-violet-100'>
       <Link href='/' passHref>
-        <div className='flex items-center pl-3 cursor-pointer'>
-          <Image src='/suzu.png' width={30} height={35} alt='鈴のアイコン' className=''/>
+        <div className='flex items-center pl-2 cursor-pointer'>
+          <Image src='/suzu2.png' width={120} height={32} alt='鈴のアイコン' className=''/>
         </div>
       </Link>
+      <Link href='/' passHref>
+        <button className='hidden sm:flex items-center py-1 px-3 hover:text-white hover:bg-purple-600 rounded-md border border-purple-700 transition cursor-pointer'>
+          TOP
+        </button>
+      </Link>
+      
     </header>
   );
 });
