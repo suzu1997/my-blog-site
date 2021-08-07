345689bc77365ec02d8c6c31ceaa0cfc37f4f8ec
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-07 16:28:42 +0900

    💄 ブログ一覧のgapを修正

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -9,7 +9,7 @@ export const BlogList: VFC<Props> = memo((props) => {
   const { blogs } = props;
   
   return (
-    <div className='grid grid-cols-blogs gap-10 justify-center md:w-3/4 text-center'>
+    <div className='grid grid-cols-blogs gap-9 justify-center md:w-3/4 text-center'>
       {blogs &&
         blogs.map((blog: any) => {
           return <BlogItem key={blog.id} blog={blog} />;
