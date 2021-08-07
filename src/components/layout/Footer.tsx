37a8246ed7f56fc14d59e763e735e09a34fe49a6
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-07 16:27:21 +0900

    🩹 コピーライトを変更

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { memo, VFC } from 'react';
 export const Footer: VFC = memo(() => {
   return (
     <footer className='flex justify-center items-center mb-20 sm:mb-0 w-full h-10 sm:h-10 text-purple-700 bg-violet-100'>
-      &copy;Chie Suzuhara 2021
+      &copy;Suzu☺︎ 2021
     </footer>
   );
 });
