8b3bd55cf474ffaa948c2b09a2da57b1f8c7cbe3
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-16 00:19:22 +0900

    📈 google analyticsの設定の変更

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -1,10 +1,26 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
 
-// PV 測定
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string): void => {
-  // GA_TRACKING_ID が設定されていない場合は、処理終了
   if (!GA_TRACKING_ID) return;
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+type GaEventProps = {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+};
+
+export const event = ({ action, category, label, value }: GaEventProps): void => {
+  if (!GA_TRACKING_ID) return;
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
   });
 };
