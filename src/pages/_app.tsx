82865646031008a062984f2c3a21f5a8e55828a8
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-08-16 00:53:48 +0900

    🐛 analyticsのscriptを/document.tsxに記載

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,26 +31,6 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        {/* // GA_TRACKING_ID が設定されていない場合は、なし */}
-        {GA_TRACKING_ID && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });`,
-              }}
-            />
-          </>
-        )}
         <title>Suzu&apos;s Diary</title>
         <meta
           name='description'
